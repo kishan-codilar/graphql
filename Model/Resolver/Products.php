706847d57d7debac98e0f195b45f0593869e3637
Kishan Kumar <kishankumar@codilar.com>
2022-02-09 15:24:33 +0530

    changed the code

--- Model/Resolver/Products.php
+++ Model/Resolver/Products.php
@@ -1,82 +1,77 @@
 <?php
-
-declare (strict_types = 1);
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
 namespace Codilar\VendorGraphQl\Model\Resolver;
 
-use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogGraphQl\Model\Resolver\Products\Query\ProductQueryInterface;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
-use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Catalog\Model\ProductRepository;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Catalog\Block\Product\Context;
-use Magento\Framework\Api\SearchCriteriaInterface;
-use Codilar\Vendor\Model\Vendor;
-use Codilar\Vendor\Model\ResourceModel\Vendor\CollectionFactory;
 
 
-class Products implements ResolverInterface {
 
-     /**
-     * @var CollectionFactory
+/**
+ * Products field resolver, used for GraphQL request processing.
+ */
+class Products implements ResolverInterface
+{
+    /**
+     * @var ProductQueryInterface
      */
-    protected $collectionFactory;
+    private $searchQuery;
 
     /**
-     * @var SearchCriteriaBuilder
+     * @var SearchCriteriaBuilders
      */
-    protected $searchCriteriaBuilder;
+    private $searchCriteriaBuilders;
 
+    /**
+     * @param PProductRepositoryInterface $productRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     */
     public function __construct(
-        ProductRepository $productRepository,
-        SearchCriteriaInterface $searchCriteriaInterface,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        CollectionFactory $collectionFactory
-    ) {
-       $this->productRepository=$productRepository;
-       $this->searchCriteriaInterface=$searchCriteriaInterface;
-       $this->searchCriteriaBuilder=$searchCriteriaBuilder;
-       $this->collectionFactory = $collectionFactory;
+
+        \Magento\Framework\Api\SearchCriteriaBuilder                   $searchCriteriaBuilders,
+
+        ProductRepositoryInterface                                    
+         $productRepository
+
+    )
+    {
+        $this->searchCriteriaBuilders = $searchCriteriaBuilders;
+        $this->productRepository = $productRepository;
+
+
     }
+
     /**
      * @inheritdoc
      */
     public function resolve(
-       Field $field,
-       $context,
-       ResolveInfo $info,
-       array $value = null,
-       array $args = null
-    ) {
-       $vendorId = $this->getVendorId($args);
-       $productData = $this->getProductData($vendorId);
-       return $productData;
-    }
-    /**
-     * @param array $args
-     * @return int
-     * @throws GraphQlInputException
-     */
-    private function getVendorId(array $args): int {
-       if (!isset($args['vendor_id'])) {
-           throw new GraphQlInputException(__('Vendor id should be specified'));
-       }
-       return (int) $args['vendor_id'];
-    }
-    /**
-     * @param int $vendorId
-     * @return array
-     * @throws GraphQlNoSuchEntityException
-     */
-    private function getProductData(int $vendorId): array
+        Field       $field,
+                    $context,
+        ResolveInfo $info,
+        array       $value = null,
+        array       $args = null
+    )
     {
-        $searchCriteriaFilter = $this->searchCriteriaBuilder->addFilter('vendor', $vendorId, 'eq')->create();
+        $searchCriteriaFilter = $this->searchCriteriaBuilders->addFilter('vendor', $args['id'], 'eq')->create();
         $productCollection = $this->productRepository->getList($searchCriteriaFilter);
-        return $productCollection->getItems();
- 
-    }
-}
+        $productRecord['items'] = [];
+        foreach ($productCollection->getItems() as $VendorProduct) {
+            $productId = $VendorProduct->getId();
+            $productRecord['items'][$productId] = $VendorProduct->getData();
+            $productRecord['items'][$productId] ['model'] = $VendorProduct;
+        }
 
+        return $productRecord;
+    }
+
+}
